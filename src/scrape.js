d779d843c25ac7beb3452fabeef014ba0031a3c7
Stevens Qiu <stevensqiu@gmail.com>
2019-02-07 22:18:43 -0800

    retrieves all sections

--- src/scrape.js
+++ src/scrape.js
@@ -1,8 +1,10 @@
 let http = require('http');
 let https = require('https');
 
-// https.globalAgent.maxSockets = 5;
-// http.globalAgent.maxSockets = 5;
+https.globalAgent.maxSockets = 50;
+http.globalAgent.maxSockets = 50;
+
+console.log (https.globalAgent.maxSockets);
 
 let Subject = require('./CourseInfo/Subject');
 let Course = require('./CourseInfo/Course')
@@ -26,7 +28,6 @@ rp(UBCCourses)
         let subjects = $('tr', tbody);
         
         subjects.each(function(i, elem) {
-
             let codeChild = $(this).children().first();
             let code = codeChild.text();
             // console.log(code);
@@ -49,7 +50,8 @@ rp(UBCCourses)
         });
 
         let promises = [];
-        for (let subject of SubjectList.slice(SubjectList.length / 2, SubjectList.length / 2 + 10)) {
+        // for (let subject of SubjectList.slice(SubjectList.length / 2, SubjectList.length / 2 + 10)) {
+        for (let subject of SubjectList) {
             if (subject.link !== undefined && subject.link !== null) {
                 // console.log(subject.link);
                 promises.push(rp('https://courses.students.ubc.ca' + subject.link)
@@ -58,6 +60,8 @@ rp(UBCCourses)
                 }));
             }
         }
+
+        console.log(SubjectList.length);
 
         return promises;
     })
@@ -94,9 +98,13 @@ rp(UBCCourses)
         }
 
         let sectionPromises = []
-        for (let course of CourseList.slice(CourseList.length / 2, CourseList.length / 2 + 10)) {
+        // for (let course of CourseList.slice(CourseList.length / 2, CourseList.length / 2 + 10)) {
+        for (let course of CourseList) {
             sectionPromises.push(rp('https://courses.students.ubc.ca' + course.course_link));
         }
+
+        console.log(CourseList.length);
+
         return sectionPromises;
     })
     .then(promises => Promise.all(promises))
@@ -124,21 +132,29 @@ rp(UBCCourses)
 
                 let status = curr_td.text();
                 curr_td = curr_td.next();
+
                 let curr_section = curr_td.text();
                 let href = $('a', curr_td).attr('href');
                 curr_td = curr_td.next();
+
                 let activity = curr_td.text();
                 curr_td = curr_td.next();
+
                 let term = curr_td.text();
                 curr_td = curr_td.next();
+
                 let interval = curr_td.text();
                 curr_td = curr_td.next();
+
                 let days = curr_td.text();
                 curr_td = curr_td.next();
+
                 let start = curr_td.text();
                 curr_td = curr_td.next();
+
                 let end = curr_td.text();
                 curr_td = curr_td.next();
+
                 let comments = curr_td.text();
 
                 let section = new Section(status, curr_section, href, activity, term, interval, days, start, end, comments);
@@ -157,20 +173,15 @@ rp(UBCCourses)
                     
                     // console.log(currCourse);
                 }
-                // console.log(section.section_number);
-                // console.log(SubjectListMap);
-                // console.log(SubjectListMap[section.subject_code].courses);
-                // let course = SubjectListMap[section.subject_code];
-                // console.log(course);
-                // SubjectListMap[section.subject_code].courses[section.course_number].sections[section.section_number] = section;
-                // console.log(section);
-                // SectionList.push(section);
             });
         }
         let innerSectionPromises = []
-        for (let section of SectionList.slice(SectionList.length / 2, SectionList.length / 2 + 10)) {
+        // for (let section of SectionList.slice(SectionList.length / 2, SectionList.length / 2 + 10)) {
+        for (let section of SectionList) {
             innerSectionPromises.push(rp('https://courses.students.ubc.ca' + section.href));
         }
+
+        console.log(SectionList.length);
         return innerSectionPromises;
         // console.log(SubjectListMap['GERM']);
     })
@@ -180,7 +191,75 @@ rp(UBCCourses)
         for (let promise of sectionPromises) {
             let $ = cheerio.load(promise);
 
+            // Obtain Subject, Course, and Section codes from page
+
+            let section = $('.active')[0].children[0].data;
+            // console.log(section);
+            let section_subject = section.split(" ")[0];
+            let section_course = section.split(" ")[1];
+            let section_section = section.split(" ")[2];
+
+            // Get Tables on Page (should contain a sectionTable, instructorTable, seatTable, bookTable)
+            let tables = $('table');
+            // console.log(mainTable);
+
+            // Add Building and Room details to Section
+            let sectionTable = tables[1];
+            // console.log(instructor_table);
+            let sectionBody = $('tbody', sectionTable);
+            let section_td = $('td', sectionBody);
+
+            let section_building = section_td.first().next().next().next().next().text();
+            let section_room = section_td.first().next().next().next().next().next().text();
+
+            SubjectListMap[section_subject].courses[section_course].sections[section_section].building = section_building;
+            SubjectListMap[section_subject].courses[section_course].sections[section_section].room = section_room;
+            // console.log(section_building);
+            // console.log(section_room);
+
+            // Add instructors to Section
+            let instructors = [];
+            let instructorTable = tables[2];
+            let instructorBody = $('tbody', instructorTable);
+            let instructorTrs = $('tr', instructorBody);
+            // console.log(instructorTrs);
+
+            instructorTrs.each(function() {
+                instructors.push($(this).children().first().next().text());
+                // console.log($(this).children().first().next().text());
+            });
+
+            // console.log(section);
+            // console.log(instructors);
+            SubjectListMap[section_subject].courses[section_course].sections[section_section].instructors = instructors;
+
+            // Add Seat Summary to Section
+            let seatTable = tables[3];
+            let seatBody = $('tbody', seatTable);
+            // console.log(seatBody);
+            let currSeat = $('tr', seatBody).first();
+            let totalRemaining = $('td', currSeat).first().next().text();
+            // console.log(totalRemaining);
+            currSeat = currSeat.next();
+            let currrentlyRegistered = $('td', currSeat).first().next().text();
+            currSeat = currSeat.next();
+            let generalRemaining = $('td', currSeat).first().next().text();
+            currSeat = currSeat.next();
+            let restrictedRemaining = $('td', currSeat).first().next().text();
+            // console.log(totalRemaining);
+            // console.log(currrentlyRegistered);
+            // console.log(generalRemaining);
+            // console.log(restrictedRemaining);
+            SubjectListMap[section_subject].courses[section_course].sections[section_section].totalRemaining = totalRemaining;
+            SubjectListMap[section_subject].courses[section_course].sections[section_section].currentlyRegistered = currrentlyRegistered;
+            SubjectListMap[section_subject].courses[section_course].sections[section_section].generalRemaining = generalRemaining;
+            SubjectListMap[section_subject].courses[section_course].sections[section_section].restrictedRemaining = restrictedRemaining;
+            // break;
+
         }
+        console.log(SubjectListMap);
+
+        return;
     })
     .catch(function (err) {
         console.log(err);
